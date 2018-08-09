4b63a3872d4f9d6a4cc7618c0fcb1e729f01f610
Evandro  Vicente de Souza <evandrovicente.souza@gmail.com>
2018-08-09 17:02:56 -0300

    Update Nivel_agua.ino

--- Nivel_agua.ino
+++ Nivel_agua.ino
@@ -1,9 +1,9 @@
 /*************************************************************************
-  Tópicos Especiais - IoT - Samuel
+  Tópicos Especiais - IoT -
   Medidor de Agua
   Indica o nível de volume de agua no recepiente
   Circuito - Potenciomero ligado ao pino A0.
-  Adaptado por Samuel - 02/07/18
+  Adaptado por  - 02/07/18
 **************************************************************************/
 #define espAgua Serial2
 #define pinResetEsp 4 
